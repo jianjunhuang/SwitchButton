1cfa53b74bddd29b71187241d5bce79da4ad4e0a
jianjunhuang <jianjun.huang@mobisummer.com>
2018-10-13 15:21:47 +0800

    clear background paint's shader when switch status with no animation

--- lib/src/main/java/com/jianjunhuang/lib/switchbutton/SwitchButton.java
+++ lib/src/main/java/com/jianjunhuang/lib/switchbutton/SwitchButton.java
@@ -293,6 +293,7 @@ public class SwitchButton extends AppCompatTextView implements Animator.Animator
       }
       mAnimator.start();
     } else {
+      mBgPaint.setShader(null);
       changeStyle();
     }
   }
